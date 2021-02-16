45c9ff1fe6d16528310a0b46cb14149905256c59
tristanmatthew <tristanmatthew@gmail.com>
2021-02-16 18:17:24 -0500

    removed punctuation and converted to lowercase

--- word_frequency.py
+++ word_frequency.py
@@ -7,7 +7,23 @@ STOP_WORDS = [
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+# 1 open file and save it to a variable - Jupyter 09
+# - normalize all words to lowercase - sentence.lower
+    with open(file) as open_file:
+        read_file = open_file.read().lower()
+# 2 remove punctuation jupyter 07
+        remove_punctuation = '''!()-[]{};:'"`\,<>./?@#$%^&;*_~'''
+        for char in read_file:
+            if char in remove_punctuation:
+                read_file = read_file.replace(char, "")
+
+    print(read_file)
+
+
+# # - remove "stop words" -- words used so frequently they are ignored
+# check each word and see if it is equal to one of the stop words/if it is in the list of stop words
+# - go through the file word by word and keep a count of how often each word is used
+    # pass
 
 
 if __name__ == "__main__":
